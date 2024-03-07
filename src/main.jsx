0b3beb57d55279373c895cca2e0bb0a3da409b37
GlebShylovich <glebshylovich10@gmail.com>
2024-03-07 09:38:28 -0800

    firebase config

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './styles/index.scss'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { initializeApp } from "firebase/app";
+import config from "../config.js";
+import { BrowserRouter } from "react-router-dom";
+import "./styles/index.scss";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+initializeApp(config);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
