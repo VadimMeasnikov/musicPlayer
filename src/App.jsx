04b4bb91084a927487f30eb67ea5dfa691560438
Arovan393 <vadimmeasnikov@gmail.com>
2024-03-13 17:54:14 +0300

    add reg

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { BrowserRouter as Router, NavLink, Route, Routes } from 'react-router-dom'
+
+import Home from './Pages/Home/Home.jsx'
+import Registration from  './Pages/Registration/Registration.jsx'
+import LogIn from './Pages/LogIn/LogIn.jsx'
+
 import './stylesGlobal/App.scss'
 
 export default function App() {
   return (
-    <div>App</div>
+    <Router>
+        <Routes>
+          <Route path='/' element={<Registration/>}/>
+          <Route path='/login' element={<LogIn/>}/>
+        </Routes>
+    </Router>
   )
 }
 
