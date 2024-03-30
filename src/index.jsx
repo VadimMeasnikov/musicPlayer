72e1b60b40dec57799389ee66505e1613949a166
GlebShylovich <glebshylovich10@gmail.com>
2024-03-30 06:23:08 -0700

    reduxToolkit

--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { initializeApp } from "firebase/app";
 import config from "../config.js";
-
+import { Provider } from "react-redux";
+import { store } from './reduxToolkit/store.js'
 import "./stylesGlobal/index.scss";
 
 initializeApp(config);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Provider store={store}>
       <App />
+    </Provider>
   </React.StrictMode>
 );
