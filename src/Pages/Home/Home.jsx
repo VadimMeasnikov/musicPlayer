2604de8ca88d3097c09b0e9943e3834674bbadef
Arovan393 <vadimmeasnikov@gmail.com>
2024-04-01 15:55:18 +0300

    add user redux

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
+import { setUser, removeUser } from '../../reduxToolkit/slices/userSlice'
+
+import { useDispatch, useSelector } from 'react-redux'
 import { useGetTrackQuery } from '../../reduxToolkit/queryApi/tracksJamendo'
+import { useAuth } from '../../hooks/useAuth'
+
 import './home.scss'
 
+
+
+
 export default function Home() {
-  const {data} = useGetTrackQuery()
-  console.log(data)
+  // const {data} = useGetTrackQuery()
+  // console.log(data)
+  const auth = getAuth()
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const getUser = useAuth()
+  console.log(getUser)
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(setUser({
+          email: user.email,
+          password: null,
+          username: user.displayName,
+          news: null,
+          share: null
+        }))
+      } else {
+        navigate('/registration')
+      }
+    })
+  })
+
   return (
-    <div>Home</div>
+    <div>
+      <button onClick={() => {
+          dispatch(removeUser())
+          navigate('/registration')
+      }}>Log out</button>
+    </div>
   )
 }
