c6f8837830dfa1e86b4bc342b915f8a367749481
GlebShylovich <glebshylovich10@gmail.com>
2024-04-05 20:29:32 +0200

    ready to merge

--- src/Components/MiniCard/MiniCard.jsx
+++ src/Components/MiniCard/MiniCard.jsx
@@ -1,15 +1,19 @@
 import './MiniCard.scss'
 import cardImg from '../../img/cardImg.png'
 
-export default function MiniCard() {
+export default function MiniCard({ item }) {
+  console.log(item.image)
   return (
     <div className="card">
-        <div className="card-img">
-            <img src={cardImg} alt="img" />
-        </div>
-        <div className="card-title">
-            <p>1(Remastered)</p>
-        </div>
+      <div className="card-img">
+        <img src={item.image} alt="img" />
+      </div>
+      <div className="card-title">
+        <audio controls>
+          <source src={item.audio} type="audio/mpeg"></source>
+        </audio>
+        <p>{item.name}</p>
+      </div>
     </div>
   )
 }
--- src/reduxToolkit/queryApi/tracksJamendo.js
+++ src/reduxToolkit/queryApi/tracksJamendo.js
@@ -3,13 +3,21 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const tracksApi = createApi({
     reducerPath: 'tracksApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://api.jamendo.com/v3.0/tracks/?client_id=354e8ba5&format=jsonpretty&limit=all&name=hello+world'
+        baseUrl: 'https://api.jamendo.com/v3.0/',
     }),
     endpoints: (builder) => ({
         getTrack: builder.query({
-            query: () => '/tracks',
-        })
-    })
+            query: (name) => ({
+                url: 'tracks/',
+                params: {
+                    client_id: '354e8ba5',
+                    format: 'jsonpretty',
+                    limit: 10,
+                    featured: 1
+                },
+            }),
+        }),
+    }),
 });
 
 export const { useGetTrackQuery } = tracksApi;
