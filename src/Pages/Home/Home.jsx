ad18879b6ecd9d92efc5e22ff54d2bdcf2bd7f0a
Arovan393 <vadimmeasnikov@gmail.com>
2024-04-05 21:42:13 +0300

    test

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React  from 'react'
 import { useGetTrackQuery } from '../../reduxToolkit/queryApi/tracksJamendo'
-import { useAuth } from '../../hooks/useAuth'
+
 
 import './home.scss'
 
 
 
 
+
 export default function Home() {
+
   const {data} = useGetTrackQuery()
   console.log(data)
+
+
   return (
     <div className="featuredTracks">
       {featured.map((item, index) => (
