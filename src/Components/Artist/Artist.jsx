32873f2b1a58706055ad17a9bdccf8a02e4b4e99
GoodMafia <danya.makagonov@internet.ru>
2024-04-20 19:20:28 +0500

    fix artists

--- src/Components/Artist/Artist.jsx
+++ src/Components/Artist/Artist.jsx
@@ -1,24 +1,19 @@
 import React from 'react';
 import { useState } from 'react';
 import defaultImg from '../../img/default.png'
-import { useDispatch } from 'react-redux';
 import { selectArtist } from '../../reduxToolkit/slices/selectedArtistsSlice'; 
 import './artist.scss'
 
 export default function Artist({ item, onSelect }) { 
-
     const [src, setSrc] = useState(item.image)
-    const dispatch = useDispatch();
-
     const handleArtistClick = () => {
-        onSelect(item); 
-        dispatch(selectArtist(item)); 
+        onSelect(item);    
     };
 
     if (src === ""){
         setSrc(defaultImg)
     }
-
+    
     return (
         <div className='artist' onClick={handleArtistClick}>
             <div><button className='choose_actor__button' ><img className='artist_photo' src={src} alt="" /></button></div>
