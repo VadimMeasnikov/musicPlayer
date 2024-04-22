c97bb35175a6f1878149127d5c660699c3b7e2e1
GlebShylovich <glebshylovich10@gmail.com>
2024-04-22 19:49:15 +0300

    regPage new styles

--- src/Pages/Registration/Registration.jsx
+++ src/Pages/Registration/Registration.jsx
@@ -1,46 +1,72 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import { createUserWithEmailAndPassword,  getAuth,  onAuthStateChanged } from "firebase/auth";
-import Registration_Data from '../../Components/Registration_Data/Registration_Data';
-import miniLogo from '../../img/mini_logo.png'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+} from "firebase/auth";
+import Registration_Data from "../../Components/Registration_Data/Registration_Data";
+import miniLogo from "../../img/spotifyLogo.png";
+import googleLogo from "../../img/googleLogo.png"
+import facebookLogo from "../../img/facebookLogo.png"
 
-import './registration.scss'
-
+import "./registration.scss";
 
 export default function Registration({ userObj }) {
+  const {
+    userEmail,
+    setUserEmail,
+    userPassword,
+    setUserPassword,
+    userName,
+    setUserName,
+    userNews,
+    setUserNews,
+    userShare,
+    setUserShare,
+  } = userObj;
 
-  const { userEmail, setUserEmail, userPassword, setUserPassword, userName, setUserName, userNews, setUserNews,
-  userShare, setUserShare } = userObj;
-
-  const [isRegistration, setIsRegistration] = useState(false)
-  const regState = { isRegistration, setIsRegistration }
-
+  const [isRegistration, setIsRegistration] = useState(false);
+  const regState = { isRegistration, setIsRegistration };
 
   return (
-    <div className='registration'>
+    <div className="registration">
       {/* <div className="registration_content"></div> */}
-      {isRegistration ?
-        (<Registration_Data regState={regState} userObj={userObj} />)
-        :
-        (<div className='reg'>
+      {isRegistration ? (
+        <Registration_Data regState={regState} userObj={userObj} />
+      ) : (
+        <div className="reg">
           <div className="reg_container">
             <div className="title_box">
-              <h1 className='reg_page__title'>Millions of Songs. Free on Music Player.</h1>
-              <img className='mini_logo' src={miniLogo} alt="logo" />
+              <img className="mini_logo" src={miniLogo} alt="logo" />
+              <h1 className="reg_page__title">
+                Millions of Songs.<br></br>Free on Spotify.
+              </h1>
             </div>
             <div className="reg_funct_box">
               <button
-                onClick={() => { setIsRegistration(true) }}
-                className='sign_up__btn'>
-                Sign Up Free
+                onClick={() => {
+                  setIsRegistration(true);
+                }}
+                className="sign_up__btn"
+              >
+                Sign up free
               </button>
-              <Link to='/login' className='log_in__link'>Log In</Link>
+              <button className="regGoogleBtn">
+                <p>Continue with Google</p>
+                <img src={googleLogo} alt="google" />
+              </button>
+              <button className="regFacebookBtn">
+                <p>Continue with Facebook</p>
+                <img src={facebookLogo} alt="facebook" />
+              </button>
+              <Link to="/login" className="log_in__link">
+                Log In
+              </Link>
             </div>
           </div>
-
-        </div>)
-
-      }
+        </div>
+      )}
     </div>
-  )
+  );
 }
